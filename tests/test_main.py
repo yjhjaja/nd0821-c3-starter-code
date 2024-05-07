888d26328bedf525f2c3f516c6a8885b69c34813
Junhui Yang <119138211+yjhjaja@users.noreply.github.com>
2024-05-07 13:03:14 +0100

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,26 +12,46 @@ def test_get():
     assert r.json() == 'Hello, welcome!'
 
 def test_post_1():
-    data = {"age": 40,
-            "workclass": "Self-emp-inc",
-            "fnlgt": 300000,
+    data = {"age": 20,
+            "workclass": "Private",
+            "fnlgt": 112847,
             "education": "HS-grad",
-            "education_num": 6,
-            "marital_status": "Married-civ-spouse",
-            "occupation": "Exec-managerial",
-            "relationship": "Wife",
-            "race": "White",
-            "sex": "Female",
-            "capital_gain": 10000,
-            "capital_loss": 10,
+            "education_num": 9,
+            "marital_status": "Never-married",
+            "occupation": "Farming-fishing",
+            "relationship": "Own-child",
+            "race": "Asian-Pac-Islander",
+            "sex": "Male",
+            "capital_gain": 0,
+            "capital_loss": 0,
             "hours_per_week": 40,
             "native_country": "United-States"
             }
     r = client.post("/inference", data = json.dumps(data))
     assert r.status_code == 200
-    assert r.json()=='0' or r.json()=='1'
+    assert r.json()=='0'
 
 def test_post_2():
+    data = {"age": 67,
+            "workclass": "Private",
+            "fnlgt": 105252,
+            "education": "Masters",
+            "education_num": 14,
+            "marital_status": "Married-civ-spouse",
+            "occupation": "Exec-managerial",
+            "relationship": "Husband",
+            "race": "White",
+            "sex": "Male",
+            "capital_gain": 0,
+            "capital_loss": 0,
+            "hours_per_week": 40,
+            "native_country": "United-States"
+            }
+    r = client.post("/inference", data = json.dumps(data))
+    assert r.status_code == 200
+    assert r.json()=='1'
+
+def test_post_3():
     data = {}
     r = client.post("/inference", json = json.dumps(data))
     assert r.status_code == 422
